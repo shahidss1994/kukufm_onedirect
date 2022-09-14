22ab598c690ed1009a31656268cbcff2fe06bc00
shahidss1994 <shahidss1994@gmail.com>
2022-09-14 17:26:27 +0530

    fix

---
+++ app/src/main/java/com/shock/kukufm_onedirect/KukuFmOneDirectApplication.kt
@@ -0,0 +1,8 @@
+package com.shock.kukufm_onedirect
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class KukuFmOneDirectApplication : Application() {
+}
